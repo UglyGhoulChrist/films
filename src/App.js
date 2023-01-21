7d5a7480a3bd713dc185da76901a2f5cdbbe2202
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2023-01-21 15:50:55 +0300

    v.1.0.1

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Stars from "./components/Stars";
 function App() {
   return (
     <div className="App">
-      <Stars count={-10} />
+      <h1>Проверяю возможные варианты</h1>
+      <h2>Рейтинг отображается, если count - 1,2,3,4,5</h2>
+      <Stars count={-1} />
       <Stars count={0} />
-      <Stars count={"1"} />
+      <Stars count={1} />
       <Stars count={2} />
+      <Stars count={3} />
       <Stars count={3.5} />
-      <Stars count={"4"} />
+      <Stars count={4} />
       <Stars count={5} />
-      <Stars count={"string"} />
+      <Stars count={"5"} />
       <Stars count={6} />
       <Stars count={null} />
       <Stars count={undefined} />
